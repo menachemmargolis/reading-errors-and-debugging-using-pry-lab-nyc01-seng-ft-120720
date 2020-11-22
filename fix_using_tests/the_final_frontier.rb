dd4e3eb9edb22eda89420f4ebeb63e6d51631400
menachem m margolis <margolismendel5728@gmail.com>
2020-11-22 21:13:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,7 +3,7 @@
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
-binding.pry
+
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
@@ -12,5 +12,5 @@ end
 def engage
   date = generate_star_date
   puts state_log(date)
-  
+  binding.pry
 end
